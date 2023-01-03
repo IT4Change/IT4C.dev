f0ead6c746ae000147637ccf86401baaf9e970eb
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-01-03 21:45:54 +0100

    renamed command `lint` to `test` to fit the npm standard new rule textlint-rule-no-exclamation-question-mark

--- .textlintrc.js
+++ .textlintrc.js
@@ -2,10 +2,13 @@ module.exports = {
   rules: {
     'no-todo': true,
     'no-start-duplicated-conjunction': {
-      interval : 2 // interval of sentences
-    }
+      interval : 2, // interval of sentences
+    },
+    'max-comma': {
+      max : 4,
+    },
   },
   filters: {
-    comments: true
-  }
+    comments: true,
+  },
 }
