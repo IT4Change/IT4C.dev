e8ff66a539e72d6b06237475ba3a29b12b15cf8c
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-01-07 04:29:52 +0100

    typo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'IT Team for Change',
-  description: 'IT4C.dev introduces the IT Team for Change Team, applied Philosophy and Projects.',
+  description: 'IT4C.dev introduces the IT Team for Change, applied Philosophy and Projects.',
   head: [
     ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1'}]
   ],
