f83bd2bd1627e7ab16a35c5ce262a82f5e5ebcf4
elweyn <heine.hannes@gmail.com>
2023-02-02 10:54:04 +0100

    Remove max 4 commatas

--- .textlintrc.js
+++ .textlintrc.js
@@ -4,9 +4,9 @@ module.exports = {
     'no-start-duplicated-conjunction': {
       interval : 2, // interval of sentences
     },
-    'max-comma': {
+    /*'max-comma': {
       max : 4,
-    },
+    },*/
     /*'no-dead-link': {
       checkRelative: true,
       baseURI: null,
