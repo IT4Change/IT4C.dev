0c9c350c0cae8bfbbda756271ef71cd772349052
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-06 14:43:25 +0100

    separate config in different files

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,79 +1,10 @@
-import { searchProPlugin } from 'vuepress-plugin-search-pro'
-import { hopeTheme } from 'vuepress-theme-hope'
 import { defineUserConfig } from 'vuepress'
+import meta from './config/meta'
+import theme from './config/theme'
+import plugins from './config/plugins'
 
 export default defineUserConfig({
-  title: 'IT Team for Change',
-  description: 'IT4C.dev introduces the IT Team for Change, applied Philosophy and Projects.',
-  head: [
-    ['meta', {name: 'viewport', content: 'width=device-width,initial-scale=1'}]
-  ],
-  theme: hopeTheme({
-    logo: '/it4c-logo2-clean-bg_alpha-128x128.png',
-    docsRepo: 'https://github.com/IT4Change/IT4C.dev',
-    docsBranch: 'master',
-    docsDir: 'docs',
-    editLink: true,
-    lastUpdated: false,
-    contributors: false,
-    navbar: [
-      { text: 'IT4C', link: '/' },
-      { text: 'Contact', link: '/contact' },
-      {
-        text: 'Github',
-        link: 'https://github.com/IT4Change'
-      },
-    ],
-    sidebar: [
-      {
-        text: 'IT4C',
-        link: '/',
-        collapsible: false,
-        children: [
-          '/history',
-          '/contact'
-        ]
-      },
-      {
-        text: 'Projects',
-        collapsible: false,
-        children: [
-          {
-            text: 'Gradido Software',
-            link: '/projects/gradido'
-          },
-          // { link: '/extras/contributing', text: 'Contributing ✨' },
-          // { link: '/extras/license', text: 'License 📚' },
-        ]
-      },
-      {
-        text: 'People',
-        collapsible: false,
-        children: [
-          '/people/alexander-friedland',
-          '/people/hannes-heine',
-          { link: '/people/mathias-lenz', text: 'Mathias Lenz' },
-          '/people/moriz-wahl',
-          '/people/ulf-gebhardt',
-          '/people/wolfgang-huss'
-        ]
-      }
-    ],
-  }),
-  plugins: [
-    searchProPlugin({
-      indexContent: true,
-      autoSuggestions: true,
-      customFields: [
-        {
-          getter: (page) => page.frontmatter.category,
-          formatter: "Category: $content",
-        },
-        {
-          getter: (page) => page.frontmatter.tag,
-          formatter: "Tag: $content",
-        },
-      ],
-    }),
-  ],
+  ...meta,
+  theme,
+  plugins,
 })
