45dbc1a79963b04605e8b6c983d0a9c227ed544e
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-11-07 09:17:18 +0100

    merge conflict resolved

--- docs/.vuepress/config/theme.js
+++ docs/.vuepress/config/theme.js
@@ -8,6 +8,7 @@ export default hopeTheme({
   editLink: true,
   lastUpdated: false,
   contributors: false,
+  footer: 'CC BY IT4C.dev & Authors. <a href="imprint.html">Imprint</a>.',
   navbar: [
     { text: 'IT4C', link: '/' },
     { text: 'Contact', link: '/contact' },
@@ -49,6 +50,6 @@ export default hopeTheme({
         '/people/ulf-gebhardt',
         '/people/wolfgang-huss'
       ]
-    }
+    },
   ],
 })
