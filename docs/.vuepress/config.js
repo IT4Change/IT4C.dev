bc7719cf44c9f8feebbf92e7d2431f808b433c87
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2024-01-29 14:21:19 +0100

    correctly use bundle option now required

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+import { viteBundler } from '@vuepress/bundler-vite'
 import { defineUserConfig } from 'vuepress'
 import meta from './config/meta'
 import theme from './config/theme'
@@ -5,4 +6,5 @@ import theme from './config/theme'
 export default defineUserConfig({
   ...meta,
   theme,
+  bundler: viteBundler({}),
 })
