baf2b067c4feb7bc3b8179f183d8e84b73b09697
Sebastian Stein <sebastian@codepassion.de>
2025-02-20 12:22:03 +0100

    - adapted Hope hero section - removed remaining Hero Widgets - added ContentSection - optimized layout of team section

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -4,13 +4,15 @@ import ProcessStep from './components/ProcessStep.vue'
 import ContactForm from './components/ContactForm.vue'
 import TeamMember from './components/TeamMember.vue'
 import ProjectCard from './components/ProjectCard.vue'
+import ContentSection from './components/ContentSection.vue'
 
 export default defineClientConfig({
   enhance({ app }) {
+    app.component('ContactForm', ContactForm)
+    app.component('ContentSection', ContentSection)
     app.component('ServiceCard', ServiceCard)
     app.component('ProcessStep', ProcessStep)
-    app.component('ContactForm', ContactForm)
-    app.component('TeamMember', TeamMember)
     app.component('ProjectCard', ProjectCard)
+    app.component('TeamMember', TeamMember)
   },
 })
