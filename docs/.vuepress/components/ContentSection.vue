37e457718f224cf88d3ad2ef9934ab56e69aaba3
Sebastian Stein <sebastian@codepassion.de>
2025-02-20 12:22:03 +0100

    - adapted Hope hero section - removed remaining Hero Widgets - added ContentSection - optimized layout of team section

---
+++ docs/.vuepress/components/ContentSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <section :class="['full-width-section', backgroundColor]">
+    <div class="content-width">
+      <div class="py-16">
+        <h1>{{ title }}</h1>
+        <slot></slot>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ContentSection',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    backgroundColor: {
+      type: String,
+      default: 'bg-transparent'
+    }
+  }
+}
+</script>
--- docs/.vuepress/components/TeamMember.vue
+++ docs/.vuepress/components/TeamMember.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <img :src="image" :alt="name" class="rounded-full mx-auto mb-4 w-48 h-48 object-cover">
+    <img :src="image" :alt="name" class="rounded-full mx-auto mb-4 w-48 h-48 object-cover grayscale">
     <h4 class="font-semibold">{{ name }}</h4>
     <p class="text-sm text-gray-600">{{ role }}</p>
   </div>
