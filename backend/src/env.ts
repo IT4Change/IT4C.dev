a102bb57dcf1c935f494ddb387ce744af4cbaeab
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-02-23 19:00:28 +0100

    backend - mail api for it4c
    Implements an fastify backend service with an email service.
    This allows to send us emails received via contact form on the website.

---
+++ backend/src/env.ts
@@ -0,0 +1,28 @@
+import { EnvType, load } from 'ts-dotenv';
+
+export type Env = EnvType<typeof schema>;
+
+export const schema = {
+    NODE_ENV: {
+        type: ['production' as const, 'development' as const],
+        default: 'development',
+    },
+    EMAIL_RECEIVER: {
+        type: String,
+        default: 'admin@it4c.dev'
+    },
+    EMAIL_SUBJECT: {
+        type: String,
+        default: 'Received EMail via IT4C'
+    },
+    PORT: {
+        type: Number,
+        default: 3000
+    }
+};
+
+export let env: Env;
+
+export function loadEnv(): void {
+    env = load(schema);
+}
---
+++ backend/src/index.ts
@@ -0,0 +1,51 @@
+import { loadEnv, env} from './env';
+loadEnv(); // Executed synchronously before the rest of your app loads
+
+import { Static, Type } from '@fastify/type-provider-typebox'
+import Fastify from 'fastify'
+import Mailer from 'nodemailer'
+
+// Nodemailer
+const mailService = Mailer.createTransport({
+	sendmail: true,
+	newline: 'unix',
+	path: '/usr/sbin/sendmail',
+})
+
+// Fatify
+const fastify = Fastify({
+  logger: true
+})
+
+// Mail
+const schema = Type.Object({
+  name: Type.String({ minLength: 2}),
+  email: Type.String({ format: 'email' }),
+  text: Type.String({ minLength: 5}),
+})
+
+type Body = Static<typeof schema>
+
+interface Reply {
+  200: { success:boolean };
+  '4xx': { success: false, error: string };
+}
+
+fastify.post<{Body: Body, Reply: Reply}>('/mail', {schema},  async (request, reply) => {
+  mailService.sendMail({
+    to: env.EMAIL_RECEIVER,
+    from: `"${request.body.name}" <${request.body.email}>`,
+    subject: env.EMAIL_SUBJECT,
+    text: request.body.text,
+  })
+
+  reply.status(200).send({success: true})
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: env.PORT })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
