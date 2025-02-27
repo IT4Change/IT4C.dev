f44d070529920369ad16164073045a47575265e3
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-02-27 09:02:48 +0100

    eslint

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,32 +1,35 @@
-import { EnvType, load } from 'ts-dotenv';
+import { load } from "ts-dotenv";
+
+import type { EnvType } from "ts-dotenv";
+
+export const schema = {
+  NODE_ENV: {
+    type: ["production" as const, "development" as const],
+    default: "development",
+  },
+  EMAIL_RECEIVER: {
+    type: String,
+    default: "admin@it4c.dev",
+  },
+  EMAIL_SUBJECT: {
+    type: String,
+    default: "[IT4C] Received EMail from %s",
+  },
+  PORT: {
+    type: Number,
+    default: 3000,
+  },
+  MAIL_HOST: {
+    type: String,
+    default: "localhost",
+  },
+};
 
 export type Env = EnvType<typeof schema>;
 
-export const schema = {
-    NODE_ENV: {
-        type: ['production' as const, 'development' as const],
-        default: 'development',
-    },
-    EMAIL_RECEIVER: {
-        type: String,
-        default: 'admin@it4c.dev'
-    },
-    EMAIL_SUBJECT: {
-        type: String,
-        default: '[IT4C] Received EMail from %s'
-    },
-    PORT: {
-        type: Number,
-        default: 3000
-    },
-    MAIL_HOST: {
-        type: String,
-        default: 'localhost'
-    }
-};
-
+// eslint-disable-next-line import/no-mutable-exports
 export let env: Env;
 
 export function loadEnv(): void {
-    env = load(schema);
+  env = load(schema);
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,63 +1,77 @@
-import { loadEnv, env} from './env';
-loadEnv(); // Executed synchronously before the rest of your app loads
+import util from "node:util";
 
-import { FormatRegistry, Static, Type, TypeBoxTypeProvider, TypeBoxValidatorCompiler } from '@fastify/type-provider-typebox'
-import Fastify from 'fastify'
-import Mailer from 'nodemailer'
-import { IsEmail } from './formats';
-import util from 'node:util'
+import {
+  FormatRegistry,
+  Type,
+  TypeBoxValidatorCompiler,
+} from "@fastify/type-provider-typebox";
+import Fastify from "fastify";
+import { createTransport } from "nodemailer";
+
+import { loadEnv, env } from "./env"; // Executed synchronously before the rest of your app loads
+import { IsEmail } from "./formats";
+
+import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+
+loadEnv();
 
 // Register EMail format
-FormatRegistry.Set('email', (value) => IsEmail(value))
+FormatRegistry.Set("email", (value) => IsEmail(value));
 
 // Nodemailer
-const mailService = Mailer.createTransport({
+const mailService = createTransport({
   host: env.MAIL_HOST,
   port: 465,
   secure: true,
-  tls: { 
-    rejectUnauthorized: false ,
+  tls: {
+    rejectUnauthorized: false,
   },
-})
+});
 
 // Fatify
 const fastify = Fastify({
-  logger: true
-}).setValidatorCompiler(TypeBoxValidatorCompiler)
+  logger: true,
+}).setValidatorCompiler(TypeBoxValidatorCompiler);
 
 // Mail
 const schema = {
   body: Type.Object({
-    name: Type.String({ minLength: 2, maxLength: 35}),
-    email: Type.String({ format: 'email' }),
-    telephone: Type.Optional(Type.String({ minLength: 8})),
-    text: Type.String({ minLength: 5}),
+    name: Type.String({ minLength: 2, maxLength: 35 }),
+    email: Type.String({ format: "email" }),
+    telephone: Type.Optional(Type.String({ minLength: 8 })),
+    text: Type.String({ minLength: 5 }),
   }),
   response: {
-    200: Type.Object({ success: Type.Boolean({default: true}) }),
-    400: Type.Object({ success: Type.Boolean({default: false}), error: Type.String() })
-  }
-}
+    200: Type.Object({ success: Type.Boolean({ default: true }) }),
+    400: Type.Object({
+      success: Type.Boolean({ default: false }),
+      error: Type.String(),
+    }),
+  },
+};
 
-fastify.withTypeProvider<TypeBoxTypeProvider>().post('/mail', {schema},  async (request, reply) => {
-  try {
-    await mailService.sendMail({
-      to: env.EMAIL_RECEIVER,
-      from: `"${request.body.name}" <${request.body.email}>`,
-      subject: util.format(env.EMAIL_SUBJECT,request.body.name),
-      text: `${request.body.text}${request.body.telephone ? `\n\nTelephone: ${request.body.telephone}` : ''}`,
-    })
-    reply.status(200).send({success: true})
-  } catch (error){
-    reply.status(400).send({success: false, error: error as string})
-  }
-})
+fastify
+  .withTypeProvider<TypeBoxTypeProvider>()
+  .post("/mail", { schema }, async (request, reply) => {
+    try {
+      await mailService.sendMail({
+        to: env.EMAIL_RECEIVER,
+        from: `"${request.body.name}" <${request.body.email}>`,
+        subject: util.format(env.EMAIL_SUBJECT, request.body.name),
+        text: `${request.body.text}${request.body.telephone ? `\n\nTelephone: ${request.body.telephone}` : ""}`,
+      });
+      return reply.status(200).send({ success: true });
+      // eslint-disable-next-line no-catch-all/no-catch-all
+    } catch (error) {
+      return reply.status(400).send({ success: false, error: error as string });
+    }
+  });
 
 // Run the server!
 try {
-  fastify.listen({ port: env.PORT })
+  void fastify.listen({ port: env.PORT });
+  // eslint-disable-next-line no-catch-all/no-catch-all
 } catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
+  fastify.log.error(err);
+  process.exit(1);
 }
-
