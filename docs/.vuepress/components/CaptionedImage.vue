e276f713ce8a67b49f8fddd7b2a412c8511a38d8
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-03-18 11:37:08 +0100

    CaptionedImage component
    Component for captioned images

---
+++ docs/.vuepress/components/CaptionedImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <figure>
+    <img :src="src" alt="" :width="width"/>
+    <figcaption>{{ caption }}</figcaption>
+  </figure>
+</template>
+<script>
+export default {
+  props: ['src', 'caption', 'width'],
+}
+</script>
+
+<style lang="scss">
+figcaption {
+    font-style: italic;
+}
+</style>
