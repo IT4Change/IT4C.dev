f36975e78cf63fad59e3d9d7af9b6c99c7be01ab
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-03-19 19:19:48 +0100

    fix(other): fix tailwind (#297)
    * remove empty main.css
    
    * fix tailwind to work properly with vuepress
    
    Have Headline paddings apply only to the blank layout.
    Change contentwidth only on blank layout.
    Do only partially import tailwind (ommit preflight) as it is in conflict
    with vuepress.
    
    * tailwind load preflight, but exclude certain styles
    
    * limit fixes to vuepress classes

--- docs/.vuepress/layouts/BlankLayout.vue
+++ docs/.vuepress/layouts/BlankLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="theme-container">
+  <div class="theme-container blank-layout">
     <Content />
     <Footer />
   </div>
@@ -10,3 +10,48 @@ export default {
   name: 'BlankLayout'
 }
 </script>
+
+<style>
+@layer theme, base, components, utilities;
+
+@import "tailwindcss/theme.css" layer(theme);
+@import "tailwindcss/preflight.css" layer(base);
+@import "tailwindcss/utilities.css" layer(utilities);
+
+/* Conflicts with vuepress */
+@layer base {
+  .vp-sidebar {
+    box-sizing: content-box;
+  }
+  .vp-nav-logo {
+    display: inline;
+  }
+}
+
+.blank-layout {
+  --content-width: 1160px;
+}
+
+.blank-layout h1, h2, h3, h4, h5, h6 {
+    font-family: var(--font-family-heading);
+  }
+
+.blank-layout h1, h2 {
+  border-bottom: none;
+  padding-bottom: 2rem;
+  font-weight: bold;
+}
+
+.blank-layout .content-width {
+  max-width: var(--content-width);
+  margin: 0 auto;
+  padding: 0 2.5rem;
+
+  @media (max-width: 768px) {
+    padding: 0 1.75rem;
+  }
+  @media (max-width: 400px) {
+    padding: 0 1rem;
+  }
+}
+</style>
