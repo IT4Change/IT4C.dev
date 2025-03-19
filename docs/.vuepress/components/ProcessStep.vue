bb22bd89c1d3cd8571b5d919f7435b98ff884878
Sebastian Stein <sebastian@codepassion.de>
2025-03-19 11:16:48 +0100

    improved ProcessStep layout

--- docs/.vuepress/components/ProcessStep.vue
+++ docs/.vuepress/components/ProcessStep.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="grid grid-cols-12 items-start mb-16">
     <div class="col-span-3 text-right pr-8">
-      <span class="text-6xl font-bold highlight-number">{{ number.padStart(2, '0') }}</span>
+      <span class="text-5xl sm:text-6xl font-bold highlight-number">{{ number.padStart(2, '0') }}</span>
     </div>
-    <div class="col-span-9 pt-4">
+    <div class="col-span-9 pt-0 sm:pt-2">
       <h3 class="text-xl font-semibold mb-2">{{ title }}</h3>
       <p class="text-gray-600">{{ description }}</p>
     </div>
