c8b7bbef93bd28b3936a139c7f6111f97bdf0f85
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-03-20 17:15:23 +0100

    eslint

---
+++ eslint.config.ts
@@ -0,0 +1,33 @@
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+export default typescriptEslint.config(
+  { ignores: ['coverage',
+                    'backend',
+                    'docs/.vuepress/.cache',
+                    'docs/.vuepress/.temp',
+                    'docs/.vuepress/dist'] },
+  {
+    extends: [
+      eslint.configs.recommended,
+      ...typescriptEslint.configs.recommended,
+      ...eslintPluginVue.configs['flat/recommended'],
+    ],
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        parser: typescriptEslint.parser,
+      },
+    },
+    rules: {
+      // your rules
+    },
+  },
+  eslintConfigPrettier
+);
