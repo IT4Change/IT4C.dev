cdb19445789419f0fc5a1536459e50385b7665c9
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2025-03-20 17:06:41 +0100

    fix(other): vuepress style fixes (#301)
    * fix several vuepress related style problems
    
    * make figure image full width
    
    - for ocelot
    - for dreammall
    
    * theme-hope-content p & ul margins

--- docs/.vuepress/layouts/BlankLayout.vue
+++ docs/.vuepress/layouts/BlankLayout.vue
@@ -28,17 +28,34 @@ export default {
   .vp-nav-logo {
     display: inline;
   }
+  .theme-hope-content p {
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
+  .theme-hope-content ul {
+    list-style: unset;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
+  .theme-hope-content figure {
+    margin: inherit;
+    padding: inherit;
+  }
 }
 
 .blank-layout {
   --content-width: 1160px;
 }
 
-.blank-layout h1, h2, h3, h4, h5, h6 {
+.blank-layout h1, .blank-layout h2, .blank-layout h3, .blank-layout h4, .blank-layout h5, .blank-layout h6 {
     font-family: var(--font-family-heading);
   }
 
-.blank-layout h1, h2 {
+.blank-layout h1, .blank-layout h2 {
   border-bottom: none;
   padding-bottom: 2rem;
   font-weight: bold;
