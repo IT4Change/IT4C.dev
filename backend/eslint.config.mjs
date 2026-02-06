20ddbe492bc3a854b78ebe9cacdd66aa3491d4bf
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-06 04:03:29 +0100

    fix lint

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,4 +1,3 @@
-// eslint-disable-next-line n/no-unpublished-import
 import config, { jest } from 'eslint-config-it4c'
 
 export default [{ ignores: ['build/', 'coverage/'] }, ...config, ...jest]
--- backend/prettier.config.mjs
+++ backend/prettier.config.mjs
@@ -1,4 +1,3 @@
-// eslint-disable-next-line n/no-unpublished-import
 import config from 'eslint-config-it4c/prettier'
 
 export default config
