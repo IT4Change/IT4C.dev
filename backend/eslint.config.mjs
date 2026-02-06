c6ce6ccc6fb447cf9bebc48f8bd8d72fc0416dad
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-06 02:54:12 +0100

    remove node_modules from ignore as it is the default for eslint9

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,4 +1,4 @@
 // eslint-disable-next-line n/no-unpublished-import
 import config, { jest } from 'eslint-config-it4c'
 
-export default [{ ignores: ['node_modules/', 'build/', 'coverage/'] }, ...config, ...jest]
+export default [{ ignores: ['build/', 'coverage/'] }, ...config, ...jest]
