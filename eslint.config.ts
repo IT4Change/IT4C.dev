16726eef522313b9aa9000fbe04dead4f3caf85f
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-14 17:05:45 +0100

    tsconfig & some eslint rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,8 +2,39 @@ import config, { vue3 } from 'eslint-config-it4c'
 
 export default [
   {
-    ignores: ['coverage', 'backend', 'docs/.vuepress/.cache', 'docs/.vuepress/.temp', 'docs/.vuepress/dist'],
+    ignores: [
+      'coverage',
+      'backend',
+      'docs/.vuepress/.cache',
+      'docs/.vuepress/.temp',
+      'docs/.vuepress/dist',
+    ],
   },
   ...config,
   ...vue3,
+  {
+    files: ['**/*.cjs'],
+    rules: {
+      'import-x/no-commonjs': 'off',
+    },
+  },
+  {
+    rules: {
+      'n/no-unpublished-import': 'off',
+      'import-x/no-extraneous-dependencies': 'off',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'any',
+          },
+        },
+      ],
+    },
+  },
 ]
