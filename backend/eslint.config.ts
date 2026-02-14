dcbb9d06701de5755e38f0e6270ade95f7257494
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2026-02-14 17:28:03 +0100

    ts include fix

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -1,3 +1,17 @@
 import config, { jest } from 'eslint-config-it4c'
 
-export default [{ ignores: ['build/', 'coverage/'] }, ...config, ...jest]
+export default [
+  { ignores: ['build/', 'coverage/'] },
+  ...config,
+  ...jest,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['jest/*.ts', 'src/*.spec.ts', 'src/*.test.ts'],
+          defaultProject: './tsconfig.eslint.json',
+        },
+      },
+    },
+  },
+]
